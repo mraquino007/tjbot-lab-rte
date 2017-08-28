1819467978023daae1ebc503f96e83486445c98b
mraquino <mraquino@br.ibm.com>
2017-08-28 14:11:33 -0300

    Initial code

---
+++ index.js
@@ -0,0 +1,13 @@
+var config = require('./config.json');
+var workspaceId = 'zzz';
+
+const TJBot = require('tjbot');
+
+var tj = new TJBot(config.hardware, config.configuration, config.credentials);
+
+tj.converse(workspaceId, "", function(response) {
+    console.log(JSON.stringify(response));
+});
+
+
+
